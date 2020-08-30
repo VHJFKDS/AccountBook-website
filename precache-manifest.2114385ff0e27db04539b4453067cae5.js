82cade4cb000cb65bbfafca352c14743d6f0d36c
Yang <651033755@qq.com>
2020-08-30 17:14:49 +0800

    deploy

---
+++ precache-manifest.2114385ff0e27db04539b4453067cae5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d673463f2054d49e96a1",
+    "url": "/AccountBook-website/css/app.e4dca25e.css"
+  },
+  {
+    "revision": "3a350d9e37f1e0fa9a5b",
+    "url": "/AccountBook-website/css/chunk-vendors.6dc57fe0.css"
+  },
+  {
+    "revision": "8926a60736b2b619b4cb56d29bd52c5c",
+    "url": "/AccountBook-website/index.html"
+  },
+  {
+    "revision": "d673463f2054d49e96a1",
+    "url": "/AccountBook-website/js/app.cfb3f54f.js"
+  },
+  {
+    "revision": "3a350d9e37f1e0fa9a5b",
+    "url": "/AccountBook-website/js/chunk-vendors.fd523cd9.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/AccountBook-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/AccountBook-website/robots.txt"
+  }
+]);
